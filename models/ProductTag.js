ca137b81e4a897d428bae5eccbea6f229c957d93
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-05-28 23:21:39 +0100

    added data to models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,32 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // references product model's id
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // references tag model's id
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
